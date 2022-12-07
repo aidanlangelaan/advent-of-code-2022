08f4c2b22f8cdaa5f8ea32f0b924326bee7e5ac1
Aidan Langelaan <aidanlangelaan@gmail.com>
2022-12-07 10:33:36 +0100

    Cleanup + prep day 08

--- src/AdventOfCode.Console/Program.cs
+++ src/AdventOfCode.Console/Program.cs
@@ -6,15 +6,15 @@ Console.WriteLine("--- Advent of Code 2022 ---");
 // var day01 = new Solver<Day01>(true);
 // day01.SolveDayPart1();
 // day01.SolveDayPart2();
-//
+
 // var day02 = new Solver<Day02>(true);
 // day02.SolveDayPart1();
 // day02.SolveDayPart2();
-//
+
 // var day03 = new Solver<Day03>(true);
 // day03.SolveDayPart1();
 // day03.SolveDayPart2();
-//
+
 // var day04 = new Solver<Day04>(true);
 // day04.SolveDayPart1();
 // day04.SolveDayPart2();
@@ -27,6 +27,10 @@ Console.WriteLine("--- Advent of Code 2022 ---");
 // day06.SolveDayPart1();
 // day06.SolveDayPart2();
 
-var day07 = new Solver<Day07>(true);
-//day07.SolveDayPart1();
-day07.SolveDayPart2();
+// var day07 = new Solver<Day07>(true);
+// day07.SolveDayPart1();
+// day07.SolveDayPart2();
+
+var day08 = new Solver<Day08>(true);
+day08.SolveDayPart1();
+day08.SolveDayPart2();
