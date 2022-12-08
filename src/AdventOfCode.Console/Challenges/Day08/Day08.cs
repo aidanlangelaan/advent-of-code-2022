4a2a8784ebd8c9dffe0e2b3cfc06ab2d4cd93b50
Aidan Langelaan <aidanlangelaan@gmail.com>
2022-12-08 11:29:59 +0100

    Day 08 - Part 1 (took wayyyy to long)

--- src/AdventOfCode.Console/Challenges/Day08/Day08.cs
+++ src/AdventOfCode.Console/Challenges/Day08/Day08.cs
@@ -13,14 +13,48 @@ public class Day08 : Challenge<Day08>
     public Day08() : base()
     {
     }
-    
+
     public override int SolvePart1()
     {
-        throw new NotImplementedException();
+        var grid = _input.Select(t => t.Select(x => int.Parse(x.ToString())).ToList()).ToList();
+        var visible = 0;
+        
+        for (var y = 1; y < grid.Count - 1; y++)
+        {
+            for (var x = 1; x < grid[y].Count - 1; x++)
+            {
+                if (IsTreeVisible(grid, x, y))
+                {
+                    visible += 1;
+                }
+            }
+        }
+        
+        visible += grid[0].Count * 2 + (grid.Count - 2) * 2;
+        return visible;
     }
 
     public override int SolvePart2()
     {
         throw new NotImplementedException();
+    }
+    
+    private static bool IsTreeVisible(IReadOnlyList<List<int>> grid, int x, int y)
+    {
+        var tree = grid[y][x];
+
+        var treesLeft = grid[y].Skip(0).Take(x);
+        var visibleLeft = !treesLeft.Any(i => i >= tree);
+        
+        var treesRight = grid[y].Skip(x + 1).Take(grid[y].Count - x);
+        var visibleRight = !treesRight.Any(i => i >= tree);
+
+        var treesTop = grid.Select(i => i[x]).Skip(0).Take(y);
+        var visibleTop = !treesTop.Any(i => i >= tree);
+        
+        var treesBottom = grid.Select(i => i[x]).Skip(y + 1).Take(grid.Count - y);
+        var visibleBottom = !treesBottom.Any(i => i >= tree);
+
+        return visibleLeft || visibleRight || visibleTop || visibleBottom;
     }
 }
--- src/AdventOfCode.Tests/Day08Tests.cs
+++ src/AdventOfCode.Tests/Day08Tests.cs
@@ -11,20 +11,24 @@ public class Day08Tests
     {
         var testInput = new[]
         {
-            "put test values here",
+            "30373",
+            "25512",
+            "65332",
+            "33549",
+            "35390",
         };
         
         _day08 = new Day08(testInput);
     }
 
     [Test]
-    public void Example_Part1_ShouldReturn123()
+    public void Example_Part1_ShouldReturn21()
     {
         // act
         var result = _day08.SolvePart1();
 
         // assert
-        Assert.That(result, Is.EqualTo(123));
+        Assert.That(result, Is.EqualTo(21));
     }
     
     [Test]
