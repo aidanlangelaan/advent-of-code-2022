bcb4949345c292b3852c1ffa43110302559867d3
Aidan Langelaan <aidanlangelaan@gmail.com>
2022-12-10 11:18:30 +0100

    Cleanup + prep day 10

---
+++ src/AdventOfCode.Console/Challenges/Day10/Day10.cs
@@ -0,0 +1,26 @@
+using System.ComponentModel;
+using AdventOfCode.Core;
+
+namespace AdventOfCode.Challenges;
+
+[Description("Day 10")]
+public class Day10 : Challenge<Day10>
+{
+    public Day10(string[] input) : base(input)
+    {
+    }
+
+    public Day10() : base()
+    {
+    }
+    
+    public override int SolvePart1()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override int SolvePart2()
+    {
+        throw new NotImplementedException();
+    }
+}
