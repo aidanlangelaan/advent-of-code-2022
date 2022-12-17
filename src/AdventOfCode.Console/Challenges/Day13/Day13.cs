b9a4de26d60c8012a8f1b7ee51a84fa75cfc85e4
Aidan Langelaan <aidanlangelaan@gmail.com>
2022-12-17 21:04:01 +0100

    Day 13 - part 1

--- src/AdventOfCode.Console/Challenges/Day13/Day13.cs
+++ src/AdventOfCode.Console/Challenges/Day13/Day13.cs
@@ -1,5 +1,7 @@
 using System.ComponentModel;
+using System.Text.Json.Nodes;
 using AdventOfCode.Core;
+using AdventOfCode.Core.Extensions;
 
 namespace AdventOfCode.Challenges;
 
@@ -13,14 +15,51 @@ public class Day13 : Challenge<Day13>
     public Day13() : base()
     {
     }
-    
+
     public override int SolvePart1()
     {
-        throw new NotImplementedException();
+        var pairs = GetPairs();
+
+        var correctPairIndexes = new List<int>();
+        foreach (var (pair, index) in pairs.WithIndex())
+        {
+            var left = pair[0] as JsonArray;
+            var right = pair[1] as JsonArray;
+
+            var result = Compare(left, right);
+            if (result < 0)
+            {
+                correctPairIndexes.Add(index + 1);
+            }
+        }
+
+        return correctPairIndexes.Sum();
+    }
+
+    private int Compare(JsonNode left, JsonNode right)
+    {
+        if (left is JsonValue && right is JsonValue)
+        {
+            return (int)left - (int)right;
+        }
+
+        var leftArray = left as JsonArray ?? new JsonArray((int)left);
+        var rightArray = right as JsonArray ?? new JsonArray((int)right);
+
+        return leftArray
+            .Zip(rightArray)
+            .Select(x => 
+                Compare(x.First, x.Second))
+            .FirstOrDefault(x => x != 0, leftArray.Count - rightArray.Count);
     }
 
     public override int SolvePart2()
     {
         throw new NotImplementedException();
     }
+
+    private IEnumerable<JsonNode[]> GetPairs() =>
+        _input.Where(x => !string.IsNullOrEmpty(x))
+            .Select(x => JsonNode.Parse(x))
+            .Chunk(2);
 }
--- src/AdventOfCode.Tests/Day13Tests.cs
+++ src/AdventOfCode.Tests/Day13Tests.cs
@@ -11,20 +11,42 @@ public class Day13Tests
     {
         var testInput = new[]
         {
-            "put test values here",
+            "[1,1,3,1,1]",
+            "[1,1,5,1,1]",
+            "",
+            "[[1],[2,3,4]]",
+            "[[1],4]",
+            "",
+            "[9]",
+            "[[8,7,6]]",
+            "",
+            "[[4,4],4,4]",
+            "[[4,4],4,4,4]",
+            "",
+            "[7,7,7,7]",
+            "[7,7,7]",
+            "",
+            "[]",
+            "[3]",
+            "",
+            "[[[]]]",
+            "[[]]",
+            "",
+            "[1,[2,[3,[4,[5,6,7]]]],8,9]",
+            "[1,[2,[3,[4,[5,6,0]]]],8,9]",
         };
         
         _day13 = new Day13(testInput);
     }
 
     [Test]
-    public void Example_Part1_ShouldReturn123()
+    public void Example_Part1_ShouldReturn13()
     {
         // act
         var result = _day13.SolvePart1();
 
         // assert
-        Assert.That(result, Is.EqualTo(123));
+        Assert.That(result, Is.EqualTo(13));
     }
     
     [Test]
